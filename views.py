97f7584b8bfd73caaa4268eabb2e172a251de2f6
Anjini Jamwal <anjini.jamwal@gmail.com>
2023-07-27 02:03:06 -0400

    initial

---
+++ views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint,render_template
+
+views = Blueprint(__name__,"views")
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+    
+@views.route('/result', methods=['POST'])
+def result():
+    return render_template('result.html')
+
+
+        
+
